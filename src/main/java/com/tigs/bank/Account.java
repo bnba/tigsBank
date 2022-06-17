7240b2a89a67df8f6061ebb744af543011fa96ae
TIGANA Bakary <bakary.tigana@gmail.com>
2022-06-17 17:08:32 +0200

    feat(account): US1 - deposit money in an account already received money

--- src/main/java/com/tigs/bank/Account.java
+++ src/main/java/com/tigs/bank/Account.java
@@ -1,11 +1,17 @@
 package com.tigs.bank;
 
+import java.time.LocalDateTime;
+
 public class Account {
 
     private Money balance;
 
     public void deposit(double d) {
-        this.balance = new Money(d);
+        if (this.balance != null) {
+            this.balance = new Money(d + this.balance.getValue());
+        } else {
+            this.balance = new Money(d);
+        }
     }
 
     public double getBalance() {
